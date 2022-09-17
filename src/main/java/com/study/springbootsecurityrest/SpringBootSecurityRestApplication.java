7e22525b6d603e713ebb4d8e07df1fd4d097b038
Sergey Voyteshenko <63860804+veseliy1chel@users.noreply.github.com>
2022-09-17 22:09:43 +0300

    Add Mapper to application class

--- src/main/java/com/study/springbootsecurityrest/SpringBootSecurityRestApplication.java
+++ src/main/java/com/study/springbootsecurityrest/SpringBootSecurityRestApplication.java
@@ -1,7 +1,9 @@
 package com.study.springbootsecurityrest;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootSecurityRestApplication {
@@ -10,4 +12,8 @@ public class SpringBootSecurityRestApplication {
         SpringApplication.run(SpringBootSecurityRestApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 }
