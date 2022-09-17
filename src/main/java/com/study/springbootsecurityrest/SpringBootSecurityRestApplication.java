7bb9ba2c49e1f7500e5caf02dc15c194bf1acd36
Sergey Voyteshenko <63860804+veseliy1chel@users.noreply.github.com>
2022-09-17 21:52:24 +0300

    (feat) create empty spring project and add db

---
+++ src/main/java/com/study/springbootsecurityrest/SpringBootSecurityRestApplication.java
@@ -0,0 +1,13 @@
+package com.study.springbootsecurityrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurityRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/springbootsecurityrest/SpringBootSecurityRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.springbootsecurityrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
