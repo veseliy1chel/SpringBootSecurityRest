b108c6ea8f6ed7b011fb6ce03d26d90c377d521d
Sergey Voyteshenko <63860804+veseliy1chel@users.noreply.github.com>
2022-09-17 22:19:52 +0300

    (fix) delete role from class
     role must be assigned during initialization

--- src/main/java/com/study/springbootsecurityrest/dto/PersonDTO.java
+++ src/main/java/com/study/springbootsecurityrest/dto/PersonDTO.java
@@ -9,7 +9,7 @@ public class PersonDTO {
     private String username;
     private String password;
 
-    private String role;
+
 
     public String getUsername() {
         return username;
@@ -18,10 +18,6 @@ public class PersonDTO {
     public void setUsername(String username) {
         this.username = username;
     }
-
-    public String getRole() {return role;}
-
-    public void setRole(String role) {this.role = role;}
 
     public String getPassword() {
         return password;
